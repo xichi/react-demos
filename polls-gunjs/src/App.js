f777c29015a2da669a2444ba9b7ce32a72d32a4d
XiChi <474504964@qq.com>
2020-07-16 23:59:51 +0800

    0

---
+++ polls-gunjs/src/App.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react'
+import Gun from 'gun/gun'
+import Todos from './Todos'
+import Chat from './Chat'
+import Json from './Json'
+import Poll from './Poll'
+
+class App extends Component {
+  constructor() {
+    super();
+    this.gun = Gun('http://api.xichi.xyz:8765/gun')
+  }
+  
+  render() {
+    return (
+      <div>
+        <h1>React Examples</h1>
+        <h2>Todo</h2>
+        <Todos gun={this.gun} />
+        <br />
+        <hr />
+        <h2>Chat</h2>
+        <Chat gun={this.gun} />
+        <br />
+        <hr />
+        <h2>Json</h2>
+        <Json gun={this.gun} />
+        <br />
+        <hr />
+        <h2>Poll</h2>
+        <Poll gun={this.gun} />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ polls-gunjs/src/Poll.js
@@ -0,0 +1,8 @@
+import React, { Component }  from 'react'
+import Gun from 'gun/gun'
+
+export default class Poll extends Component {
+  render(){
+    return (<div>Poll</div>)
+  }
+}
